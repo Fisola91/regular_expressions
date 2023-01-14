9aae3b44d425af55206c052e3f6f035ad1d5f1f7
Fisola Adesuyi <adesuyifisola@gmail.com>
2023-01-14 08:46:04 +0100

    rspect test for only valid email

--- lib/marketing_campaign.rb
+++ lib/marketing_campaign.rb
@@ -1,3 +1,8 @@
 def valid?(email)
   email.match?(/\w+@\w+\.\w{3}/)
 end
+
+def clean_database(email)
+
+
+end
--- spec/marketing_campaign_spec.rb
+++ spec/marketing_campaign_spec.rb
@@ -16,6 +16,16 @@ RSpec.describe "marketing campaign" do
         expect(valid?(email)).to eq(result)
       end
     end
-
   end
+
+    context "when array of valid emails" do
+      it "returns an array with valid emails only" do
+        let(:valid_email_array) {[]}
+        EMAIL.each do |email, result|
+          valid_email_array << email if EMAIL[email] == true
+        end
+        expect(clean_database(EMAIL.keys)).to match_array(valid_email_array)
+      end
+
+    end
 end
